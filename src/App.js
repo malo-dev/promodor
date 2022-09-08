64d101cbe05dcce78fe193ceaeefdf6b9b099911
malo-dev <leadermushio377@gmail.com>
2022-09-08 20:56:39 +0200

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [state, setState] = useState({
+    breakLengh: 5,
+    sessionLenght : 25
+  })
+  
+
+  
+  const downBreaklength = () => {
+    
+    setState((prevstate) => {
+      return {
+        breakLengh: prevstate.breakLengh - 1,
+         sessionLenght : 25
+      }
+    })
+  }
+  
+  const upBreaklength = () => {
+    setState((prevstate) => {
+      return {
+        breakLengh: prevstate.breakLengh+1,
+         sessionLenght : 25
+      }
+    })
+  }
+ 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container1">
+        
+        <div>
+          <h1>25 + 5 Clock</h1>
+        </div>
+        
+        <div className="container2">
+          <p>Break Length</p>
+   
+          <button onClick={downBreaklength}>Down</button>
+             {state.breakLengh <= 1 ? <p>1</p> : state.breakLengh >= 60 ? <p>60</p>:<p>{ state.breakLengh}</p>}
+          <button onClick={upBreaklength}>Up</button>
+        </div>
+        
+        <div className="container3">
+          <p>Session Length</p>
+          <button>Down</button>
+          <p>{ state.sessionLenght}</p>
+          <button>Up</button>
+        </div>
+        
+        <div className='container4'>
+          <p>Session</p>
+          <h1>25 <span> : </span>00</h1>
+          
+          <div>
+            <button>Play</button>
+            <button>Break</button>
+            <button>Reload</button>
+          </div>
+          
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
